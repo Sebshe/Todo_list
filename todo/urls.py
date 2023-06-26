9b5d11f9eb3f628a5cd8f4e21dca72989d920182
Petro Abramov <sebshe3@gmail.com>
2023-06-26 16:45:17 +0300

    Rewrited path in urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -15,16 +15,16 @@ from .views import (
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
-    path("task/create", TaskCreateView.as_view(), name="task-create"),
-    path("task/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
-    path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 
     path("tags/", TagListView.as_view(), name="tag-list"),
-    path("tags/create", TagCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 
-    path('chng_st/<int:pk>', complete_undo_task, name='task_change_status'),
+    path('chng_st/<int:pk>/', complete_undo_task, name='task_change_status'),
 
 ]
 
